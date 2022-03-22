ef5d2b5612302d50f702e02a7cec46a389ff4517
= <callum.powys@gmail.com>
2022-03-22 22:12:54 +0000

    Made test case stream bit twiddling in deflate comptime

--- src/deflate.zig
+++ src/deflate.zig
@@ -86,12 +86,15 @@ const BitStream = struct {
 };
 
 test "simple deflate stream" {
-    var testData = [_]u8{ 0b00011110, 0b11111000, 0b10111100, 0b01100011, 0b10011100, 0b10001000, 0b00000000, 0b00000000, 0b00110000, 0b01000000, 0b00001100, 0b11010100, 0b10101101, 0b01001010, 0b01111000, 0b11111111, 0b01101001, 0b00011100, 0b01101000, 0b01101001, 0b00111010, 0b01111000, 0b00101001, 0b11010011, 0b10110110, 0b10000000 };
-    var inputStream = testData[0..];
+    comptime var testData = [_]u8{ 0b00011110, 0b11111000, 0b10111100, 0b01100011, 0b10011100, 0b10001000, 0b00000000, 0b00000000, 0b00110000, 0b01000000, 0b00001100, 0b11010100, 0b10101101, 0b01001010, 0b01111000, 0b11111111, 0b01101001, 0b00011100, 0b01101000, 0b01101001, 0b00111010, 0b01111000, 0b00101001, 0b11010011, 0b10110110, 0b10000000 };
 
-    for (inputStream[0..]) |*byte| {
-        byte.* = @bitReverse(u8, byte.*);
+    comptime { // can't be asked rewriting the above array to have the bits in the right order
+        for (testData[0..]) |*byte| {
+            byte.* = @bitReverse(u8, byte.*);
+        }
     }
+
+    var inputStream = testData[0..];
 
     var allocator = std.testing.allocator;
 
